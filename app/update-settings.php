2691dc8df617f599e24f84e2500bf6b7cd865e04
harpreetsgill <harpreet_singh_gill@outlook.com>
2020-11-04 06:39:47 -0800

    Settings styled

--- app/update-settings.php
+++ app/update-settings.php
@@ -12,17 +12,19 @@
 
         $user_id = $_SESSION['user_id'];
         $new_username = $_POST['user_new_username'];
-        $new_password = $_POST['user_new_password'];
+        $new_password = password_hash($_POST['user_new_password'], PASSWORD_DEFAULT);
 
         $sql = "UPDATE users
                 SET user_username = ?, user_password = ?
                 WHERE users.user_id = $user_id";
 
         $update_record = $mysqli->prepare($sql);
-        $update_record->bind_param('ss', $new_username, password_hash($new_password, PASSWORD_DEFAULT));
+        $update_record->bind_param('ss', $new_username, $new_password);
         $update_record->execute();
 
         echo 'if chaleya';
+        header('Location: ' . SITE_URL . 'settings.php' . '?settings=success');
+
 
     }
 
--- partials/settings-container.php
+++ partials/settings-container.php
@@ -6,12 +6,20 @@
     </div>
 
     <!-- Main Settings -->
-    <form method="POST" action="app/update-settings.php">
-        <label>Username</label>
-        <input type="text" name="user_new_username">
-        <input type="password" name="user_new_password">
+    <form id="form-settings" method="POST" action="app/update-settings.php">
+        <label>Name</label>
+        <input class="input-field" type="text">
 
-        <input type="submit" value="Change" name="btn-change">
+        <label>Username</label>
+        <input class="input-field" type="text" name="user_new_username">
+
+        <label>Password</label>
+        <input class="input-field" type="password" name="user_new_password">
+
+        <label>Retype Password</label>
+        <input class="input-field" type="password">
+
+        <input class="input-button" type="submit" value="Change" name="btn-change">
     </form>
 
 </div>
