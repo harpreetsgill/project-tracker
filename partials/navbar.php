34c415b0479606d449ba588441c2d62aa45d8320
harpreetsgill <harpreet_singh_gill@outlook.com>
2020-11-05 02:04:16 -0800

    Styled Username and Settings link

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,18 +1,25 @@
-<header>
-    <a href="<?php echo SITE_URL; ?>">
-        <div id="logo">
-            <img src="img/logo.svg" alt="Aivei's logo">
+<header id="flex">
+    <div id="logo">
+        <a href="<?php echo SITE_URL . 'dashboard.php'; ?>">
+        
+            <div><img src="img/logo.svg" alt="Aivei's logo"></div>
+        </a>
             <?php
                 if (SITE_URL . substr($_SERVER['REQUEST_URI'], 17) == SITE_URL){
-                    echo '<h1 id="h1-brandname">aivei</h1>';
+                    echo '<div><h1 id="h1-brandname">aivei</h1></div>';
                 }  
             ?>
-        </div>
-    </a>
-    <div>
+        
+        
+    </div>
         <?php
             if (SITE_URL . substr($_SERVER['REQUEST_URI'], 17) == SITE_URL){
                 echo '<h2 id="h2-subtitle">helps you keep track of your school projects</h2>';
+                echo '
+                <script>
+                    document.getElementById("flex").style.justifyContent = "flex-start";
+                </script>
+                ';
             }
             else {                
                 if (isset(($_SESSION['user_id']))) {
@@ -27,9 +34,8 @@
 
                     $row = $result->fetch_assoc();
 
-                    echo '<h2 id="logged-user">Hello ' . $row['user_username'] . '</h2>' . '<a href="' . SITE_URL . 'settings.php">Settings</a>';
+                    echo '<div id="div-username-setting"><h2 id="logged-user">Hello ' . $row['user_username'] . '</h2>' . '<a id="a-setting" href="' . SITE_URL . 'settings.php"><img src="img/settings.svg" alt="settings icon"></a></div>';
                 }
             }
         ?>  
-    </div>
 </header>
